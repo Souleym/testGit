25e85107eaa44cfb341de9ef97a3a8df9ddb7dda
u21402056 <u21402056@WIN060.auth.local>
2015-01-22 10:46:22 +0100

    modification ajout nom

--- TestGit/src/testGit/testIHM.java
+++ TestGit/src/testGit/testIHM.java
@@ -5,6 +5,11 @@ package testGit;
  *
  * @author u21402056
  */
+
+
 public class testIHM {
     
+    public static void main(String[] args){
+        System.out.println("Je m'appelle Souleymane");
+    }
 }
